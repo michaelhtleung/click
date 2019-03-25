7d103205b5b6dc66b5d6d5082bc6fed37c3fb29d
Michael Leung <michael@alertlabs.com>
2019-03-24 21:40:10 -0400

    clean up comments

--- javascripts.js
+++ javascripts.js
@@ -1,16 +1,10 @@
-// function removeTitle(elementId) {
-// 	var element = document.getElementById(elementId);
-// 	element.parentNode.removeChild(element);
-// }
-// alert("load js");
-
 $(document).ready(function() {
 	// wild card character selects all elements on the page
 	// which means the fadeOut() starts for any clicked element
 	// on the page
 	$("*").click(function() { 
-		$("#introText").fadeOut("slow", function() {
-			// alert("fade out complete");
-		});
+		// no need to rip things out of the DOM now, because
+		// fadeOut() sets display:none last
+		$("#introText").fadeOut("slow"); 
 	});
 });
