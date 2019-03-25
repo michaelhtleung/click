3841a1516cd6709c3ccaf5796cc89398326c9e59
Michael Leung <michael@alertlabs.com>
2019-03-24 21:36:16 -0400

    gave up and used jQuery to remove title and add fadeOut to title

--- javascripts.js
+++ javascripts.js
@@ -1,4 +1,16 @@
-function removeTitle(elementId) {
-	var element = document.getElementById(elementId);
-	element.parentNode.removeChild(element);
-}
+// function removeTitle(elementId) {
+// 	var element = document.getElementById(elementId);
+// 	element.parentNode.removeChild(element);
+// }
+// alert("load js");
+
+$(document).ready(function() {
+	// wild card character selects all elements on the page
+	// which means the fadeOut() starts for any clicked element
+	// on the page
+	$("*").click(function() { 
+		$("#introText").fadeOut("slow", function() {
+			// alert("fade out complete");
+		});
+	});
+});
