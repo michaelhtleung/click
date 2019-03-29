99fcdeeaa380312b1e4305e442938b26d01a8269
Michael Leung <michael@alertlabs.com>
2019-03-28 21:34:45 -0400

    restore accidentally deleted work

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let clickedPoints = []; // points clicked by user used to set regression parameters
+
 $(document).ready(function() {
 	// wild card character selects all elements on the page
 	// which means the fadeOut() starts for any clicked element
@@ -8,3 +10,21 @@ $(document).ready(function() {
 		$("#introText").fadeOut("slow"); 
 	});
 });
+
+function getCoordinates(event) {
+	var x = event.clientX;
+	var y = event.clientY;
+	var coords = {x, y};
+	clickedPoints.push(coords);
+	// alert(coords);
+	// console.log(coords);
+	console.log(clickedPoints);
+	// next steps:
+	// 1. append coords to dataPoints array
+	// 2. call calculateRegression() to calculate the new regression coefficients
+	// 3. call renderRegression() to update the DOM to reflect the new regression
+	// these 3 above steps can done in a getMouseWheel() function parallel to
+	// getCoordinates(), where scrolling the mouse wheel causes the order of the
+	// regression polynomial to change, but we should prompt the user to scroll
+	// their wheel or they're unlikely to find this feature
+}
