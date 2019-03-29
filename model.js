44206db7b9e87837bfbf1188892a5a30e0ece17a
Michael Leung <michael@alertlabs.com>
2019-03-29 00:00:18 -0400

    clean up comments and add regression calculation tests

--- model.js
+++ model.js
@@ -1,14 +1,3 @@
-// const regression = require('./node_modules/regression');
-// import regression from './node_modules/regression';
 let clickedPoints = []; // points clicked by user used to set regression parameters
 let model; 
 
-// module.exports = clickedPoints;
-// module.exports = model;
-
-// fast and loose testing:
-clickedPoints = [ [0,0], [1,1], [2,2] ];
-model = regression.linear(clickedPoints);
-console.log(model);
-console.log(model.predict(5));
-console.log(model.predict(2.4));
